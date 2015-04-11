748d131875a3c66ebcfe98a2ff3043db1a47a668
djherbis <djherbis@gmail.com>
2015-04-11 13:17:19 -0400

    adding a test for Get

--- atime_test.go
+++ atime_test.go
@@ -25,6 +25,25 @@ func TestStat(t *testing.T) {
 	}
 }
 
+func TestGet(t *testing.T) {
+	f, err := ioutil.TempFile("", "")
+	et := time.Now().Add(-time.Second)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		t.Error(err.Error())
+	}
+	at := Get(fi)
+	if at.Before(et) {
+		t.Errorf("expected atime to be recent: got %v instead of ~%v", at, et)
+	}
+}
+
 func TestStatErr(t *testing.T) {
 	_, err := Stat("badfile?")
 	if err == nil {
