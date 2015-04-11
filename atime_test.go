67eac24a20c51b61c0d0f41edf81fd589d8985c4
djherbis <djherbis@gmail.com>
2015-04-11 13:12:30 -0400

    atime first commit

---
+++ atime_test.go
@@ -0,0 +1,33 @@
+package atime
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestStat(t *testing.T) {
+	f, err := ioutil.TempFile("", "")
+	et := time.Now().Add(-time.Second)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	at, err := Stat(f.Name())
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if at.Before(et) {
+		t.Errorf("expected atime to be recent: got %v instead of ~%v", at, et)
+	}
+}
+
+func TestStatErr(t *testing.T) {
+	_, err := Stat("badfile?")
+	if err == nil {
+		t.Error("expected an error")
+	}
+}
