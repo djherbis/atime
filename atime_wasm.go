4c4823b49cd23c702405b8118d24045a1364f85b
wwwzbwcom <zbwhome@outlook.com>
2021-04-21 18:25:46 +0800

    fix: fix url

--- atime_wasm.go
+++ atime_wasm.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// http://golang.org/src/os/stat_wasm.go
+// http://golang.org/src/os/stat_js.go
 
 package atime
 
